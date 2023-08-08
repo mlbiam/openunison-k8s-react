8bfb797c5baff5d37e29654cb280c7efa5fdbc3f
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2023-08-08 14:54:17 -0400

    add token

--- build.sh
+++ build.sh
@@ -8,6 +8,20 @@ npm run build
 cd ../../
 pwd
 
+
+cp configs/config-dist.json html/token/src/config/config.json
+cd html/token
+pwd
+npm install
+npm run build
+cd ../../
+pwd
+
+
 rm -rf build
 mkdir -p build/scale
 mv html/scale/build/* build/scale/
+mkdir -p build/token
+mv html/token/build/* build/token/
+
+
---
+++ html/token/src/theme.js
@@ -0,0 +1,25 @@
+import { red } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+import configData from './config/config.json'
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#AC1622',
+      dark: '#780f17',
+      light: '#bc444e',
+
+    },
+    secondary: {
+      main: '#16aca0',
+      dark: '#0f7870',
+      light: '#44bcb3',
+    },
+    error: {
+      main: red.A400,
+    }
+  },
+});
+
+export default theme;
